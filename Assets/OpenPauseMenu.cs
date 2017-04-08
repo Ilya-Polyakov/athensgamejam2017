736d653592ea4ea73261fb91997a4855a7398a40
decafcoffey <coffeysd63@yahoo.com>
2017-04-08 12:51:12 -0400

    UI Update

---
+++ Assets/OpenPauseMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenPauseMenu : MonoBehaviour {
+	public GameObject menu;
+	public GameObject winnerMenu;
+	bool value = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			menu.SetActive(!value);
+			value = !value;
+		}
+		//testing purposes
+		if(Input.GetKeyDown(KeyCode.F10)){
+			winnerMenu.SetActive(!value);
+			value = !value;
+		}
+	}
+}
---
+++ Assets/UI_Quit_Prompt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Quit_Prompt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void QuitGame() {
+		Debug.Log("Quit");
+		Application.Quit();
+	}
+}
